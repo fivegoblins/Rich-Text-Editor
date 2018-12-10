7e207e50418783c61f97b1216140b7d3c58d5ff3
Alexandra <alexswartz9753@gmail.com>
2018-12-10 12:23:08 -0500

    bold mark is toggle-able

--- slate-react-rich-text-editor/src/components/TextEditor.js
+++ slate-react-rich-text-editor/src/components/TextEditor.js
@@ -43,7 +43,7 @@ export default class TextEditor extends Component {
 
         switch (event.key) {
             case 'b': {
-                change.addMark('bold')
+                change.toggleMark('bold')
                 return true
             }
         }
